85a30d1be36fe86bad9068c9673c856118bf166f
Daniel Kingery <danielkingery0@gmail.com>
2024-10-08 21:13:50 -0600

    add(testing) first playwright test and coverage

---
+++ tests/pizza.spec.js
@@ -0,0 +1,7 @@
+import { expect, test } from "playwright-test-coverage";
+
+test("home page", async ({ page }) => {
+    await page.goto("/");
+
+    expect(await page.title()).toBe("JWT Pizza");
+});
